8459a9fb2f89d911575cf13fb03e5b5fae0ff8da
lowellosaurus <LowellWood@gmail.com>
2016-03-20 21:40:58 +0430

    Add options to change to change token text, include token in the output text, and format the output text.

--- find-todos.pl
+++ find-todos.pl
@@ -2,24 +2,36 @@
 use strict;
 use warnings FATAL => 'all';
 
-# TODO: Possible flags:
-# --todo-text "TODO: "
-# --include-todo-text
-# --output-format "* {LINE_NUM}: {TODO}"
-# --force-single-line
+use Getopt::Long;
 
-my $TODO_TEXT = "TODO:";
+my $token      = 'TODO: ';
+my $format     = '{LINE_NUM}. {LINE}';
+my $file       = 0;     # default: false  
+my $incl_token = 1;     # default: true
+# TODO: Add unbreak-lines option.
+# my $rem_breaks = 0;     # default: false
+
+GetOptions(
+    'token=s'       => \$token,
+    'format=s'      => \$format,
+    'file=s'        => \$file,
+    'include-token' => \$incl_token,
+) or die "Error in command line arguments\n";
 
 my $todos = [];
 my $todo_index = 0;
 
+open(my $fh, "<", $file)
+    or die "Cannot open file '$file': $!";
+
 my $prev_pretext;
 my $line_num = 0;
-while (my $line = <>) {
+while (my $line = <$fh>) {
     $line_num++;
     chomp $line;
 
-    my ($pretext, $text) = $line =~ m/(^.*)($TODO_TEXT.*$)/i;
+    # TODO: If we find the token again, make it a new todo.
+    my ($pretext, $text) = $line =~ m/(^.*)$token(.*$)/i;
     $prev_pretext = $pretext
         if $pretext;
 
@@ -41,12 +53,22 @@ while (my $line = <>) {
     if (!$todos->[$todo_index]) {
         push @$todos, { lines => [], line_num => $line_num };
     }
+
+    # Add the token text if --include-token and first line.
+    $text = $token . $text
+        if $incl_token && !scalar(@{$todos->[$todo_index]->{lines}});
+
     push @{$todos->[$todo_index]->{lines}}, $text;
 }
 
 foreach my $todo (@$todos) {
-    $line_num = $todo->{line_num};
+    my $line_no = $todo->{line_num};
     foreach my $todo_line (@{$todo->{lines}}) {
-        print $line_num++ . ". $todo_line\n";
+        my $line = "$format\n";
+        $line =~ s/{LINE_NUM}/$line_no/g;
+        $line =~ s/{LINE}/$todo_line/g;
+
+        print $line;
+        $line_no++;
     }
 }
